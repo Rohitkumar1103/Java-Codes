9554e21661f7aaba27643259937b528a340bcb5f
Rohitkumar1103 <149893953+Rohitkumar1103@users.noreply.github.com>
2024-05-02 22:53:44 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
-public class sum{
+public class reverse{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the last number: ");
     int n = sc.nextInt();
-    int sum = 0;
-    int i = 1;
-    while (i <= n){
-      sum = sum+i;
-      i++;
+    while (n >0){
+      int lastDigit = n%10;
+      System.out.print(lastDigit);
+      n = n/10;    
     }
-    System.out.print("Sum of Natural Number upto"+ n +" : " +sum);
-}}
+  }
+}
