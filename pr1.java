a7558c45b8ccf5aa5cf9d87d2c2a5ffdb4b231d9
Rohitkumar1103 <149893953+Rohitkumar1103@users.noreply.github.com>
2024-05-05 19:29:59 +0530

    Add files via upload

--- pr1.java
+++ pr1.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
 public class pr1{
   public static void main(String[] args){
-    int count = 1;
-    while(count <= 100 ){
-      System.out.println("Hello "+count);
-      count++;
-    }
+    int marks[] = new int[100];
+    Scanner sc = new Scanner(System.in); 
+    marks[0] = sc.nextInt();
+    marks[1] = sc.nextInt();
+    marks[2] = sc.nextInt();
+
+    System.out.println("Phy: " +marks[0]);
+    System.out.println("Chem: " +marks[1]);
+    System.out.println("Math: " +marks[2]);
+
+    marks[2] = 78;
+
+    System.out.println("Math: " +marks[2]);
+    System.out.println("Length of array= " +marks.length);
   }
 }
