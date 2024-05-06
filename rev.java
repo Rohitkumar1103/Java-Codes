362086d5c8f0780bdf7d013eee6d7c031b23c9f5
Rohitkumar1103 <149893953+Rohitkumar1103@users.noreply.github.com>
2024-05-06 09:07:40 +0530

    Add files via upload

--- rev.java
+++ rev.java
@@ -1,15 +1,24 @@
-import java.util.*;
-
 public class rev{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number: ");
-    int n = sc.nextInt();
-    int rev=0;
-    while (n > 0){
-      int lastDigit = n%10;
-      rev  = (rev*10)+lastDigit;
-      n = n/10;
+  public static void reverse(int arr[]){
+    int start=0;
+    int end = arr.length - 1;
+
+    while(start<end){
+      //swap
+      int temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+
+      start++;
+      end--;
     }
-    System.out.print(rev);
-}}
+  }
+  public static void main(String[] args){
+    int arr[] = {2, 54, 23, 98, 21 , 11, 40};
+    reverse(arr);
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+ " ");
+    }
+    System.out.println();
+  }
+}
