138bcc11f2feb9b2a4283cf80bfc41dc164b0279
Rohitkumar1103 <149893953+Rohitkumar1103@users.noreply.github.com>
2024-05-27 14:04:59 +0530

    Add files via upload

--- pr1.java
+++ pr1.java
@@ -1,17 +1,20 @@
 import java.util.*;
 
 public class pr1{
-  public static void printLetters(String str){
-    for(int i=0; i< str.length(); i++){
-      System.out.print(str.charAt(i)+ " ");
-    }
-    System.out.println();
-  }
   public static void main(String[] args){
-    String firstName = "Rohit";
-    String lastName = "Kumar";
-    String fullName = firstName +" "+ lastName;
-    // System.out.println(fullName.charAt(4));
-    printLetters(fullName);
+    int n=3, m=3;
+    int matrix [][] = new int[n][m];
+    Scanner sc = new Scanner(System.in);
+    for(int i=0; i<n; i++){
+      for(int j=0; j<m; j++){
+        matrix[i][j] = sc.nextInt();
+      }
+    }
+    for(int i=0; i<n; i++){
+      for(int j=0; j<m; j++){
+        System.out.print(matrix[i][j] +" ");
+      }
+      System.out.println();
+    }
   }
 }
