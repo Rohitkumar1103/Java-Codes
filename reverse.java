25391f71678479988dad3b0074050b1278a29fa4
Rohitkumar1103 <149893953+Rohitkumar1103@users.noreply.github.com>
2024-06-16 23:22:14 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,12 +1,57 @@
-import java.util.*;
-
 public class reverse{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the last number: ");
-    int n = sc.nextInt();
-    for (int i=n;i>0;i--){
-      System.out.print(i+ " ");
-    }
+  public static class Node{
+    int data;
+    Node next;
 
-}}
+    public Node(int data){
+    this.data = data;
+    this.next = next;
+    } 
+  }
+  public static Node head;
+  public static Node tail;
+
+  public void addLast(int data){
+    Node newNode = new Node(data);
+    if(head == null){
+      head = tail = newNode;
+      return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
+
+  public void print(){
+    Node temp = head;
+    while(temp != null){
+      System.out.print(temp.data+"-> ");
+      temp = temp.next;
+    }
+    System.out.println("null");
+  }
+  public void rev(){
+    Node prev = null;
+    Node curr = tail = head;
+    Node next;
+
+    while(curr != null){
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    head = prev;
+  }
+  public static void main(String[] args){
+    reverse ll = new reverse();
+    ll.addLast(6);
+    ll.addLast(3);
+    ll.addLast(7);
+    ll.addLast(2);
+    ll.addLast(9);
+    ll.print();
+
+    ll.rev();
+    ll.print();
+  }
+}
