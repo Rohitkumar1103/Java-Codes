f1d1b63afda14aaea50762b53f49003f758a33ca
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-07-24 23:45:56 +0530

    Add files via upload

---
+++ usingTwoStack.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+public class usingTwoStack {
+
+    static class Queue {
+
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        public static int remove() {
+            if (s1.isEmpty()) {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+
+        public static int peek() {
+            if (s1.isEmpty()) {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(4);
+        q.add(7);
+        q.add(1);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
