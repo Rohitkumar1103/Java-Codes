aa18f31f8a8cf7a3679989ffa812d8252dddfe03
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-07-27 15:32:58 +0530

    Add files via upload

--- usingJCF.java
+++ usingJCF.java
@@ -6,6 +6,7 @@ public class usingJCF {
     public static void main(String[] args) {
         //Queue q = new Queue();
         Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
 
         q.add(1);
         q.add(5);
